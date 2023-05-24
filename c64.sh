d2381de50831a3985e7396b7ec5632bbd11f83a7
byteproject <stefan@8-bit.info>
2023-05-24 20:53:05 +0200

    color scheme change

--- c64.sh
+++ c64.sh
@@ -8,6 +8,6 @@ rm ${STORY}_c64.d64
 
 #compile
 ${WRAPPER} -5 ${STORY}.inf
-ruby Interpreters/Ozmoo/make.rb -t:c64 -dc:6:9 -cc:8 -ss1:"${LABEL}" -ss2:"Interactive Fiction" -ss3:"${SUBTITLE}" -sw:6 ${STORY}.z5
+ruby Interpreters/Ozmoo/make.rb -t:c64 -dc:2:3 -ss1:"${LABEL}" -ss2:"Interactive Fiction" -ss3:"${SUBTITLE}" -sw:6 ${STORY}.z5
 
 mv c64_${STORY}.d64 ${STORY}_c64.d64
