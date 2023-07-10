5b3b060ec89eb85e7f330d20aa10c37db6e96fa4
Stefan Vogt <stefan@8-bit.info>
2023-07-10 03:31:07 +0200

    unified test script for Terminal Frotz

--- test.sh
+++ test.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-# builds Commodore 64 release (without Koala Loader) but launches Windows Frotz for testing
+# Puny BuildTools - Linux testing script
+# builds z5 version and launches Frotz in current Terminal
 
 #read config file 
 source config.sh
 
 #cleanup
-rm ${STORY}_c64.d64
+rm ${STORY}.z5
 
 #compile
-${WRAPPER} ${STORY}.inf
-read -n 1 -p "Press ANY KEY to create D64 and launch Frotz..."
 ${WRAPPER} -5 ${STORY}.inf
-ruby Interpreters/Ozmoo/make.rb -t:c64 -dc:6:9 -sc:8 -cc:8 -ic:8 -ss1:"${LABEL}" -ss2:"Interactive Fiction" -ss3:"${SUBTITLE}" -sw:6 ${STORY}.z3
+read -n 1 -p "Press ANY KEY to launch Frotz..."
 
-mv c64_${STORY}.d64 ${STORY}_c64.d64
-
-#run Frotz with the freshly generated Z-file (path defined in your bash.rc)
-/mnt/c/Program\ Files\ \(x86\)/RetroTools/WindowsFrotz/Frotz.exe ${STORY}.z3
+#run Frotz with the freshly generated Z-file
+#frotz -d ${STORY}.z5
+frotz ${STORY}.z5
