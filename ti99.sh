c713531e1241406f346b20b4c65a3a4607d3edb2
Stefan Vogt <stefan@8-bit.info>
2023-07-21 20:55:39 +0200

    bugfixes

--- ti99.sh
+++ ti99.sh
@@ -14,21 +14,6 @@ ${WRAPPER} ${STORY}.inf
 cp Interpreters/ti994a.dsk ./
 mv ti994a.dsk ${STORY}_ti99.dsk
 
-# define minimum file size
-minsize=65536
-
-# get file size of the story file
-filesize=$(stat -c%s "${STORY}.z3")
-
-# padd the story file with zeroes if smaller than the minimum value of 65536 bytes
-if (( filesize < maxsize )); then
-    mv ${STORY}.z3 to ${STORY}_chunk.z3
-    dd if=/dev/zero of=${STORY}_temp.z3 ibs=1k count=64
-    dd if=${STORY}_chunk.z3 of=${STORY}_temp.z3 conv=notrunc
-    mv ${STORY}_temp.z3 to ${STORY}.z3
-    rm ${STORY}_chunk.z3
-fi
-
 #create TI99/4a story chunks
 z3toTI994A ${STORY}.z3
 
