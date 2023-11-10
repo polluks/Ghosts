fb3196dd95e1505cdc3b453453029aacb57b62d0
Stefan Vogt <stefan@8-bit.info>
2023-11-10 17:33:24 +0100

    all changes

--- c64.sh
+++ c64.sh
@@ -4,10 +4,17 @@
 #read config file 
 source config.sh
 
+#cleanup
 rm ${STORY}_c64.d64
+
+#copy resources
+cp Resources/loadpic.kla .
 
 #compile
 ${WRAPPER} -5 ${STORY}.inf
-ruby Interpreters/Ozmoo/make.rb -t:c64 -dc:2:9 -ss1:"${LABEL}" -ss2:"Interactive Fiction" -ss3:"${SUBTITLE}" -sw:6 ${STORY}.z5
+ruby Interpreters/Ozmoo/make.rb -t:c64 -i loadpic.kla -dc:2:9 -ss1:"${LABEL}" -ss2:"Interactive Fiction" -ss3:"${SUBTITLE}" -sw:6 ${STORY}.z5
 
 mv c64_${STORY}.d64 ${STORY}_c64.d64
+
+#post-cleanup
+rm loadpic.kla
