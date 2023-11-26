2f10d16f740a884e700e1cb7e17920723ad748a5
Stefan Vogt <stefan@8-bit.info>
2023-11-27 00:28:43 +0100

    z5 is shit, drop it

--- apple2.sh
+++ apple2.sh
@@ -4,22 +4,11 @@
 #read config file 
 source config.sh
 
-# #cleanup
-# rm ${STORY}_apple2.dsk
-
-# # #compile
-# ${WRAPPER} ${STORY}.inf
-
-# # #create disk image
-# interlz3 ./Interpreters/info3k.bin ${STORY}.z3 ${STORY}_apple2.dsk
-
 #cleanup
-rm ${STORY}_apple2_s1.dsk
-rm ${STORY}_apple2_s2.dsk
+rm ${STORY}_apple2.dsk
 
 #compile
-${WRAPPER} -5 ${STORY}.inf
+${WRAPPER} ${STORY}.inf
 
 #create disk image
-interlz5 ./Interpreters/info5a.bin ${STORY}.z5 ${STORY}_apple2_s1.dsk
-interlz5 ./Interpreters/info5a.bin ${STORY}.z5 ${STORY}_apple2_s1.dsk ${STORY}_apple2_s2.nib
+interlz3 ./Interpreters/info3k.bin ${STORY}.z3 ${STORY}_apple2.dsk
