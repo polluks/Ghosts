fe4b547fa1891b45203b96645ad88118ae1a8fb5
stefan.vogt@byteproject.net <stefan.vogt@byteproject.net>
2023-12-10 02:57:07 +0100

    all script update

--- all.sh
+++ all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # builds the game for all systems
 
+# Z-machine version 5 systems (standard config)
 bash c64.sh
 bash apple2.sh
 bash bbc_acorn.sh
@@ -8,20 +9,22 @@ bash speccy.sh
 bash amiga.sh
 bash plus4.sh
 bash macintosh.sh
-#bash ti99.sh
 bash atari_st.sh
-#bash oric.sh
 bash mega65.sh
 bash msx.sh
-#bash vic20_pet.sh
 bash cpc_pcw.sh
 bash c128.sh
 bash dos.sh
 bash sam_coupe.sh
-#bash trs_coco_dragon64.sh
 bash a8bit.sh -1
+bash trs80_m3.sh -2
+bash trs80_m4.sh -2
+
+# Z-machine version 3 only systems (enable if desired)
 #bash decrainbow.sh
 #bash kaypro.sh
 #bash osborne1.sh
-bash trs80_m3.sh -1
-bash trs80_m4.sh -1
+#bash trs_coco_dragon64.sh
+#bash vic20_pet.sh
+#bash oric.sh
+#bash ti99.sh
