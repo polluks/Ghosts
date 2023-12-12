5e5e3d53a94a678ff13dc6a1043dade5c9996f35
stefan.vogt@byteproject.net <stefan.vogt@byteproject.net>
2023-12-12 11:36:40 +0100

    minor formatting change

--- bundle.sh
+++ bundle.sh
@@ -26,10 +26,12 @@ while getopts ':t:h' opt; do
         cp Interpreters/CPM_Plus_speccy.dsk .
         cp -R Interpreters/DOS .
       
-        # by standard all possible z5/multi-z targets are enabled, all z3-only targets are disabled (see below)
+        # by standard all possible z5/multi-z targets are enabled, 
+        # all z3-only targets are disabled (see below)
         zip -r ${STORY}_${RELEASE}.zip ${STORY}_apple2_s1.dsk ${STORY}_apple2_s2.dsk ${STORY}_speccy.dsk ${STORY}_amiga.adf ${STORY}_atari8bit.atr ${STORY}_c128.d71 ${STORY}_plus4.d64 ${STORY}_c64.d64 ${STORY}_mega65.d81 ${STORY}_cpc_pcw.dsk ${STORY}_atarist.st ${STORY}.z5 ${STORY}_bbc_elk.ssd ${STORY}_MSX.dsk CPM_Plus_speccy.dsk ${STORY}_mac.dsk ${STORY}_sam_coupe.cpm ${STORY}_trs80_m3.dsk ${STORY}_trs80_m4.dsk Pro-DOS-v2.dsk PlayIF.pdf readme.txt licenses.txt game.transcript DOS
 
-        # these targets only support z3 and are deactivated by default, add them to the zip line above if desired
+        # these targets only support z3 and are deactivated by default, 
+        # add them to the zip line above if desired
         # ${STORY}.z3 ${STORY}_ti99.dsk ${STORY}_oric_1.dsk ${STORY}_oric_2.dsk ${STORY}_vic20_pet.d64 ${STORY}_trs_coco.dsk ${STORY}_dragon64.vdk dragon64_loader.vdk ${STORY}_osborne1.cpm ${STORY}_kaypro.cpm ${STORY}_decrainbow.cpm
 
         rm PlayIF.pdf
