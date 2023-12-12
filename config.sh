8ee666961a1a0f69033790d5f0041e2dd576014e
stefan.vogt@byteproject.net <stefan.vogt@byteproject.net>
2023-12-12 12:11:42 +0100

    config change

--- config.sh
+++ config.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # Puny BuildTools 2.0
-# configuration file
+# (c) 2023 Stefan Vogt
 
-#your inform source file without .inf suffix --- ${STORY}
+########################################
+# ---- project configuration file ---- #
+########################################
+
+#your inform source file without .inf suffix ---- ${STORY}
 STORY="ghosts"
 
-#will only be used for bundling the release .zip archive --- ${RELEASE}
+#will only be used for bundling the release archive ---- ${RELEASE}
 RELEASE="R11"
 
-#subtitle (it is recommended using your itch.io URL) --- ${SUBTITLE}
+#subtitle (it is recommended using your itch.io URL) ---- ${SUBTITLE}
 SUBTITLE="8bitgames.itch.io"
 
-#the label you want to release your software under, e.g. Infocom --- ${LABEL}
+#the label you want to release your software under, e.g. Infocom ---- ${LABEL}
 LABEL="Moonmist / ZZAP!64"
 
-#the wrapper you're going to use --- ${WRAPPER}
+#the wrapper you're going to use, pi6 is standard ---- ${WRAPPER}
 WRAPPER="pi6"
