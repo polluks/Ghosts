494aeac62b14dd498cfec9a058124a18a8bcb4b1
Stefan Vogt <stefan@8-bit.info>
2023-12-23 23:26:03 +0100

    a thousand changes

--- dos.sh
+++ dos.sh
@@ -1,17 +1,74 @@
 #!/bin/bash
-# builds MS-DOS release
+# Puny BuildTools v2.0
+# dos.sh - MS-DOS release builder
+# (c) 2023 Stefan Vogt
 
 #read config file 
 source config.sh
 
+echo -e "\ndos.sh - MS-DOS release builder"
+echo -e "Puny BuildTools 2.0, (c) 2023 Stefan Vogt\n"
+
+zmachine3()
+{
+    ${WRAPPER} -3 ${STORY}.inf
+    if ! [ -f ${STORY}.z3 ] ; then
+        echo -e "Inform 6 compiler error detected, aborting operation."
+        echo -e "MS-DOS release failed to build.\n"
+        exit 1;
+    fi
+    cp ${STORY}.z3 STORY.DAT
+}
+zmachine5()
+{
+    ${WRAPPER} -5 ${STORY}.inf
+    if ! [ -f ${STORY}.z5 ] ; then
+        echo -e "Inform 6 compiler error detected, aborting operation."
+        echo -e "MS-DOS release failed to build.\n"
+        exit 1;
+    fi
+    cp ${STORY}.z5 STORY.DAT
+}
+while getopts 35h opts
+do
+	case $opts in
+		2) zmachine3
+		;;
+        5) zmachine5
+        ;;
+		*) echo -e "\nSynopsis: ./dos.sh -3 for Z-machine version 3\n          ./dos.sh -5 for Z-machine version 5\n"
+        exit 0;
+		;;
+	esac
+done
+if [ $OPTIND -eq 1 ]; then 
+    echo -e "Synopsis: ./dos.sh -3 for Z-machine version 3\n          ./dos.sh -5 for Z-machine version 5\n\nNothing to be done. No options passed.\n"; 
+    exit 0;
+fi
+
+#cleanup
+rm -rf Releases/DOS
+
 #copy resources
-cp ./Interpreters/FROTZ.EXE ./Releases/DOS/MOONM.EXE
+cp Interpreters/FROTZ.EXE Interpreters/DOSTEMP/MOONM.EXE
 
-#compile
-${WRAPPER} -5 ${STORY}.inf
+#place story in temporary directory
+mv STORY.DAT Interpreters/DOSTEMP
 
-#prepare story 
-cp ${STORY}.z5 STORY.DAT
+#copy content to Release directory
+cp -r Interpreters/DOSTEMP Releases/DOS
 
-#place story in release directory
-mv STORY.DAT ./Interpreters/DOS/
+#check for loading screen and arrange resources
+if ! [ -f Resources/SCREEN.PNG ] ; then
+    rm Releases/DOS/PLAY.BAT
+    rm Releases/DOS/PICTVIEW.CFG
+    rm Releases/DOS/VIEWER.EXE
+    mv Releases/DOS/PLAYONLY.BAT Releases/DOS/PLAY.BAT
+    echo "No SCREEN.PNG found in /Resources dir."
+    echo -e "MS-DOS release without loading screen successfully built.\n"
+else
+    cp Resources/SCREEN.PNG Releases/DOS
+    rm Releases/DOS/PLAYONLY.BAT
+    echo "SCREEN.PNG found in /Resources dir."
+    echo -e "MS-DOS release with loading screen successfully built.\n"
+fi
--- trs80_m3.sh
+++ trs80_m3.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 # builds Tandy TRS-80 Model 3 release
 
-# This script needs a WSL2 environment to work out of the box, which is the
-# suggested environment for the Puddle Buildtools anyway. The script executes
-# a Windows binary, something perfectly fine in the WSL ecosystem. If you're
-# working from a Linux-only environment, you need to install Wine and change
-# the 'powershell' lines to 'wine cmd.exe /c' (untested) and you need to 
-# alter the paths. Why using a Windows tool? There is literally no program
-# available for Linux that allows writing files to TRS80 Model III & 4 disks.
-# Well, there is but it's not working. If you are able to point me to a
-# working solution for Linux, I'll update this script accordingly.
-#
-# NOTE FOR WSL2 USERS: The tools I am using here are TRSWRITE and TRSREAD by
-# Matthew Reed. These programs are not allowed being redistributed but they
-# are freeware and you may grab them from: 
-#
-# http://www.trs-80emulators.com/trsread-trswrite. 
-#
-# I stored them in a dedicated folder "FictionTools" on my Windows partition
-# (C:/FictionTools). The environment variable $toolswin had been defined in 
-# my .bashrc and it points to '/mnt/c/FictionTools' for easy operations.
-# Please consider that the path to your Powershell installation might differ.
-# I am using Powershell 7 and the script is confirmed to work well with it.
-
 #read config file 
 source config.sh
 
